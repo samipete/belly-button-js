eebc0aaf2f2c7c89c461652707b6caee43b02999
Sarah <116603987+samipete@users.noreply.github.com>
2023-03-20 14:43:12 -0700

    daily commit

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -36,17 +36,18 @@ function buildCharts(value, data){
     const metaData = data.metadata.find(sample=> sample.id.toString() === value)
     barChart(sampleData)
     bubbleChart(sampleData)
+    buildDemoInfo(metaData)
     console.log(sampleData)
     console.log(metaData)
 }
 // Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
 function barChart (data){
     // declare varaibles 
-     let  ids = data.otu_ids;
+     let barIds = data.otu_ids;
      let labels = data.otu_labels.slice(0, 10);
-     let values = data.sample_values.slice(0,10);
+     let values = data.sample_values.slice(0,10).reverse();
      // create y labels 
-     let yData = ids.slice(0,10);
+     let yData = barIds.slice(0,10);
      let yLabels = [];
      yData.forEach(function(ylabel){
          yLabels.push(`OTU ${ylabel.toString()}`);
@@ -55,7 +56,7 @@ function barChart (data){
      // create trace for bar chart
      let barTrace = {
         x: values,
-        y: yLabels,
+        y: yLabels.reverse(),
         text: labels,
         type: "bar",
         orientation: "h"
@@ -79,32 +80,45 @@ function barChart (data){
 
 // define funtion for bubble chart (add to build charts )
 function bubbleChart(data){
-    let ids = data.otu_ids;
+    let bubbleIds = data.otu_ids;
     let bubbleLabels = data.otu_labels;
     let bubbleValues = data.sample_values;
     let bubbleTrace = {
-        x: ids,
+        x: bubbleIds,
         y: bubbleValues,
         text: bubbleLabels,
         mode: 'markers',
         marker: {
-            size: bubbleLabels,
-            color: ids
+            size: bubbleValues,
+            color: bubbleIds
         }
     };
     let bubbleLayout = {
-        xaxis: {title: "OTU ID"}
+        xaxis: {title: "OTU ID"},
+        height: 600,
+        width: 1200
     };
     // plotly to plot bubbleTrace with layout
-
       Plotly.newPlot("bubble", [bubbleTrace], bubbleLayout);
 
 }
 
-
-
-// define function for demographics tables
-
+// define function for demographics table
+function buildDemoInfo(data){
+    d3.json('samples.json').then((data) => {
+        // Selecting the #sample-metadat html to update the information and grabbing the metadata
+                let demoTable = d3.select('#sample-metadata');
+                let demoData = data.metadata;
+                let info = demoData.filter(demoObject => demoObject.id == samples)[0];
+                // clear exisitng metadat
+                demoTable.html("");
+        
+        // use object.entreis to enter metadata, size h4, changing the KEY to uppercase and returning all labels and metadata for the ID
+                Object.entries(info).forEach(([key, type]) => {
+                    demoTable.append('h6').text(`${key}: ${type}`);
+                });
+            });
+        }
 
 
 
